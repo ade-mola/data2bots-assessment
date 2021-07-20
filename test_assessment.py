21ce1536047b749fefb50a857d6603bf8272168c
Ademola Kunmi <salamiolokun@gmail.com>
2021-07-20 22:44:57 +0100

    tests

---
+++ test_assessment.py
@@ -0,0 +1,25 @@
+"""
+@author: Ademola Olokun
+"""
+
+from data2bots_assessment import obsolete
+import pandas as pd
+
+def test_obsolete_true():
+    """
+    tests that cobsolete function returns TRUE
+    when a date less than 2021-01-01 is passed
+    """
+    date = pd.to_datetime('2020-09-01')
+    result = obsolete(date)
+    assert result == 'True'
+
+
+def test_obsolete_false():
+    """
+    tests that obsolete function returns FALSE
+    when a date greater than 2021-01-01 is passed
+    """
+    date = pd.to_datetime('2021-07-12')
+    result = obsolete(date)
+    assert result == 'False'
